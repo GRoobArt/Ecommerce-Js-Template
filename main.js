47c7efafb9870101e200166c111c6b2e335177b3
Robert Garcia <rbert.g.ch@gmail.com>
2023-07-09 17:13:19 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+window.addEventListener('scroll', () => {
+  const header = document.getElementById('header')
+  const headerHeight = header.offsetHeight
+  const asideProduct = document.getElementById('info-product')
+
+  const scrollValue =
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop
+
+  if (scrollValue > headerHeight * 1.75) {
+    header.classList.add('_sticky')
+    asideProduct.classList.add('_sticky')
+  } else {
+    header.classList.remove('_sticky')
+    asideProduct.classList.remove('_sticky')
+  }
+})
