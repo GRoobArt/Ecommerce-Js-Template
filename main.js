19db344e9abb59d783d1f2e3942927511dc00f54
Robert Garcia <rbert.g.ch@gmail.com>
2023-08-05 18:34:09 -0400

    V2.01 Add Javascripts Ciclos && Condicional

--- main.js
+++ main.js
@@ -1,18 +1,91 @@
-window.addEventListener('scroll', () => {
-  const header = document.getElementById('header')
-  const headerHeight = header.offsetHeight
-  const asideProduct = document.getElementById('info-product')
+// 1era Entrega:
 
-  const scrollValue =
-    window.pageYOffset ||
-    document.documentElement.scrollTop ||
-    document.body.scrollTop
+/**
+ * [] Crear un Algoritmo con condional.
+ * [] Crear un algoritmo utilizando un ciclo.
+ * [] Armar un simulador interactivo, la estructura final de tu proyecto integrador.
+ */
 
-  if (scrollValue > headerHeight * 1.75) {
-    header.classList.add('_sticky')
-    asideProduct.classList.add('_sticky')
-  } else {
-    header.classList.remove('_sticky')
-    asideProduct.classList.remove('_sticky')
-  }
-})
+const products = [
+  {
+    id: 1,
+    name: 'Tees Orange',
+    color: 'Orange',
+    size: 'L',
+    price: 26190,
+  },
+  {
+    id: 2,
+    name: 'Tees Blue',
+    color: 'Blue',
+    size: 'S',
+    price: 64266,
+  },
+  {
+    id: 3,
+    name: 'Tees Teal',
+    color: 'Teal',
+    size: 'XS',
+    price: 49160,
+  },
+  {
+    id: 4,
+    name: 'Tees Khaki',
+    color: 'Khaki',
+    size: 'XS',
+    price: 81753,
+  },
+  {
+    id: 5,
+    name: 'Tees Aquamarine',
+    color: 'Aquamarine',
+    size: 'XS',
+    price: 73628,
+  },
+]
+
+const cart = {
+  id: 21213,
+  iduser: 12,
+  qty: 0,
+  total: 0,
+  products: [],
+}
+
+const addProductCart = () => {
+  let addProductsContinue = true
+  do {
+    let ProductColor = prompt(
+      'Cual es el color la polera que quiere comprar ? Orange | Blue | Teal | Khaki | Aquamarine'
+    )
+    let product = products.find((p) => p.color === ProductColor)
+    let existCart = cart.products.some((p) => p.color === product.color)
+
+    if (!product) {
+      alert(`El producto con el color ${ProductColor} ingresado no existe`)
+    } else if (existCart) {
+      alert(
+        `El producto con el color ${ProductColor} ya esta ingresado en el carro`
+      )
+    } else {
+      cart.products.push(product)
+      cart.qty = Number(cart.products.length)
+      cart.total += Number(product.price)
+
+      if (addProductsContinue) {
+        let continueProduct = prompt(
+          'Quieres seguir añadiendo Productos? Si | No'
+        )
+        if (continueProduct === 'Si') {
+          addProductsContinue = true
+        } else {
+          addProductsContinue = false
+        }
+      }
+    }
+  } while (cart.qty < products.length && addProductsContinue != false)
+  // console.log(cart)
+  // console.log(addProductsContinue)
+}
+
+addProductCart()
